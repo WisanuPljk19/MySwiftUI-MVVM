20d5f861a7ac2e63c41a06317a65d69d843030aa
wisanu.pljk <wisanu.pljk@gmail.com>
2020-10-05 09:51:14 +0700

    Initial Commit

---
+++ MySwiftUI-MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySwiftUI-MVVM
+//
+//  Created by Wisanu Paunglumjeak on 5/10/2563 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
